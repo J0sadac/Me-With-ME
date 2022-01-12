6aff2b1db206a1d85e8ebb7c5f298c6c6863367d
Josadac Castañeda <jugdmentc12@gmail.com>
2022-01-11 19:12:12 -0600

    entrega del proyecto para el segundo checkpoint

--- main.js
+++ main.js
@@ -1,4 +1,35 @@
-var carName = Volvo;
-var x = 50;
-var myName = "Josadac castañeda";
+'use strcit'
 
+var formulario = document.querySelector("#formNotas");
+
+formulario.addEventListener('submit', function(){
+    var texto = document.querySelector("#addNotas").value;
+    if(texto.length >= 1){
+        localStorage.setItem(texto, texto);
+
+    }
+});
+
+var div = document.querySelector("#notas")
+
+for(var i in localStorage){
+    if(typeof localStorage[i] == 'string'){
+        var p = document.createElement("div");
+        p.classList="nota-cont"
+        p.append(localStorage[i]);
+        div.append(p)
+    }
+}
+
+
+//eliminar notar (intento de jsjsjsjsjsjsjs)
+
+/*
+formulario.addEventListener('submit', function(){
+    var texto = document.querySelector("#addNotas").value;
+    if(texto.length >= 1){
+        localStorage.removeItem(texto, texto);
+
+    }
+});
+*/
